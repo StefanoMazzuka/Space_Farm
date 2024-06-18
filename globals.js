46ba11e8d0e38484595205aeeaf25d491d2be681
Stefano Mazzuka <stefano.mazzuka@gmail.com>
2024-06-18 17:37:31 +0200

    testing

--- globals.js
+++ globals.js
@@ -10,6 +10,13 @@ var current_field = 0;
 var current_seed  = 0;
 var seeds_list    = ['wheat', 'lettuce', 'corn', 'tomato', 'dragon fruit']
 
+var seeds = {
+    wheat:        { name: 'wheat',        buy_price: 100,  sell_price: 12,  seed_stock_count: 7,   harvest_stock_count: 17 },
+    lettuce:      { name: 'lettuce',      buy_price: 200,  sell_price: 24,  seed_stock_count: 19,  harvest_stock_count: 29 },
+    corn:         { name: 'corn',         buy_price: 400,  sell_price: 48,  seed_stock_count: 43,  harvest_stock_count: 53 },
+    tomato:       { name: 'tomato',       buy_price: 800,  sell_price: 96,  seed_stock_count: 91,  harvest_stock_count: 101 },
+    dragon_fruit: { name: 'dragon fruit', buy_price: 1600, sell_price: 192, seed_stock_count: 187, harvest_stock_count: 197 }
+  }
 // market demand
 var market_demand_wheat;
 var market_demand_lettuce;
